3248ab1b11162304e9ebe01a0c7a94cc5b3b6619
Keifer <144736945+kbuss26@users.noreply.github.com>
2024-01-25 09:52:57 -0700

    1st solution of code.js
    Implements binary search by recursion in binarySearch function

--- code.js
+++ code.js
@@ -1,3 +1,32 @@
+/*
+    Keifer Buss 
+    COSC-3020-01 
+    Last modified Jan 25 2024
+    Sources: 
+    - https://github.com/COSC3020/mystery-func (slicing lists)
+*/
+
 function binarySearch(list, element) {
-    return -1;
+    var i = list.length;
+    switch(i) {
+        case 0: // Empty list
+            return -1;
+        case 1: // One element inside list; test for said element
+            return ((element == list[0]) ? 0 : -1);
+        default: // Other cases
+            var midPos = Math.floor((list.length) / 2); // Define middle position
+            if (element == list[midPos]) { return midPos; }  // Element in middle position check
+
+            // Recursive cases
+            var accum = 0; // Accumulator value for index return value if found
+            var result;
+            if (element < list[midPos]) { // Element less than middle value
+                result = binarySearch(list.slice(0, midPos), element);
+            } else { // Element greater than middle value (equal has already been checked)
+                accum += midPos;
+                result = binarySearch(list.slice(midPos, list.length), element);
+            }
+            return ((result == -1) ? -1 : (accum + result)); 
+            // Returns -1 if not found or index if found in recursive calls
+        }
 }
